8aa317a2024d5461814d39341a94ab857d6af7c3
winfreyj <jbwinfrey@gmail.com>
2018-06-28 14:38:39 -0400

    Added comments to uartConfig.c functions

--- uartConfig.c
+++ uartConfig.c
@@ -31,52 +31,52 @@ void UART0_init(void)
 
 void EUSCIA0_Interrupt_Enable(void)
 {
-	MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
+	MAP_Interrupt_enableInterrupt(INT_EUSCIA0);  // Enables EUSCI_A0 interrupts
 }
 
 void Master_Interrupt_Enable(void)
 {
-	MAP_Interrupt_enableMaster();
+	MAP_Interrupt_enableMaster();	// Enables master interrupts
 }
 
 void EUSCIA0_IRQHandler(void)
 {
-	if(EUSCI_A0->IV & 0x02)
+	if(EUSCI_A0->IV & 0x02)	// Clears the EUSCI_A0->IFG register based on the received interrupt
 	{
-		int value = (int)(EUSCI_A0->RXBUF);
+		int value = (int)(EUSCI_A0->RXBUF);	// Reads in the character from the terminal window
 
 		char buffer[50];
-		sprintf(buffer,"Value: %c\n\r", value);
+		sprintf(buffer,"Value entered: %c\n\r", value);	// Prints out the value entered
 		UART_Send_String(buffer);
 
 		if(value == 49)
 		{
-			IR_Flag = 1;
+			IR_Flag = 1;  // Sets the IR_Flag to get IR readings
 		}
 		else if(value == 50)
 		{
-		    servo_triggered = 1;
+		    servo_triggered = 1;	// Sets the servo_triggered flag to start servo motor movement
 		}
 	}
 	else
 	{
-		char* string = "Invalid input.\n\r";
+		char* string = "Invalid input.\n\r";	// Prints out invalid input if input is not a 1 or a 2
 		UART_Send_String(string);
 	}
 }
 
 void UART_Send(char c)
 {
-	while((EUSCI_A0->IFG & 0x02) == 0);
-	EUSCI_A0->TXBUF = c;
+	while((EUSCI_A0->IFG & 0x02) == 0);	// Waits while the buffer is empty
+	EUSCI_A0->TXBUF = c;	// Sends the character across the buffer
 }
 
 void UART_Send_String(char *string)
 {
 	int i , len = strlen(string);
-	for(i=0; i<len; i++)
+	for(i=0; i<len; i++)	// Goes through the entire string
 	{
-		while((EUSCI_A0->IFG & 0x02) == 0);
-		EUSCI_A0->TXBUF = *string++;
+		while((EUSCI_A0->IFG & 0x02) == 0);	// Waits while the buffer is empty
+		EUSCI_A0->TXBUF = *string++;	// Sends the character across the buffer
 	}
 }
